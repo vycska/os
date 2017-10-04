a07badba0cadf11039b825913547f32626596bda
Vytas <vycska@gmail.com>
2017-10-04 21:03:08 +0300

    corrected 'status' value in i2c structure

--- inc/i2c.h
+++ inc/i2c.h
@@ -6,7 +6,8 @@ struct I2C {
 		slave, //slave address
 		active_buffer, //0 or 1 [metadata or data]
 		direction; //0 - write, 1 - read, 2 - write and then read
-    int current,length[2],status;
+    int current,length[2];
+    volatile int status;
 };
 
 void I2C0_Init(void);
