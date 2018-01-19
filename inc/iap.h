5c35b187d91229d564284df140b9e0522418230d
Vytas <vycska@gmail.com>
2018-01-19 16:52:10 +0200

    iap.h lygiavimas (reikejo make'o testavimui)

--- inc/iap.h
+++ inc/iap.h
@@ -1,31 +1,31 @@
 #ifndef IAP_H
 #define IAP_H
 
-#define IAP_ROM_LOCATION								(0x1fff1ff1)
-#define IAP_EXECUTE_CMD(a,b)						((void(*)(unsigned int*,unsigned int*))(IAP_ROM_LOCATION))(a,b)
+#define IAP_ROM_LOCATION                  (0x1fff1ff1)
+#define IAP_EXECUTE_CMD(a,b)              ((void(*)(unsigned int*,unsigned int*))(IAP_ROM_LOCATION))(a,b)
 
-#define IAP_CMD_PREPARE_SECTORS					50
-#define IAP_CMD_COPY_RAM_TO_FLASH				51
-#define IAP_CMD_ERASE_SECTORS						52
-#define IAP_CMD_BLANK_CHECK_SECTORS			53
-#define IAP_CMD_READ_PART_ID						54
-#define IAP_CMD_READ_BOOT_ROM_VERSION		55
-#define IAP_CMD_COMPARE									56
-#define IAP_CMD_REINVOKE_ISP						57
-#define IAP_CMD_READ_SERIAL_NUMBER			58
+#define IAP_CMD_PREPARE_SECTORS           50
+#define IAP_CMD_COPY_RAM_TO_FLASH         51
+#define IAP_CMD_ERASE_SECTORS             52
+#define IAP_CMD_BLANK_CHECK_SECTORS       53
+#define IAP_CMD_READ_PART_ID              54
+#define IAP_CMD_READ_BOOT_ROM_VERSION     55
+#define IAP_CMD_COMPARE                   56
+#define IAP_CMD_REINVOKE_ISP              57
+#define IAP_CMD_READ_SERIAL_NUMBER        58
 
-#define IAP_STA_CMD_SUCCESS							0
-#define IAP_STA_INVALID_COMMAND					1
-#define IAP_STA_SRC_ADDR_ERROR					2
-#define IAP_STA_DST_ADDR_ERROR					3
-#define IAP_STA_SRC_ADDR_NOT_MAPPED			4
-#define IAP_STA_DST_ADDR_NOT_MAPPED			5
-#define IAP_STA_COUNT_ERROR							6
-#define IAP_STA_INVALID_SECTOR					7
-#define IAP_STA_SECTOR_NOT_BLANK				8
-#define IAP_ST_SECTOR_NOT_PREPARED			9
-#define IAP_STA_COMPARE_ERROR						10
-#define IAP_STA_BUSY										11
+#define IAP_STA_CMD_SUCCESS               0
+#define IAP_STA_INVALID_COMMAND           1
+#define IAP_STA_SRC_ADDR_ERROR            2
+#define IAP_STA_DST_ADDR_ERROR            3
+#define IAP_STA_SRC_ADDR_NOT_MAPPE        4
+#define IAP_STA_DST_ADDR_NOT_MAPPED       5
+#define IAP_STA_COUNT_ERROR               6
+#define IAP_STA_INVALID_SECTOR            7
+#define IAP_STA_SECTOR_NOT_BLANK          8
+#define IAP_ST_SECTOR_NOT_PREPARED        9
+#define IAP_STA_COMPARE_ERROR             10
+#define IAP_STA_BUSY                      11
 
 int sector(unsigned int);
 int iap_prepare_sectors(unsigned int, unsigned int);
